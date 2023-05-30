bfa691a5c15c2e2dddfc561bca7a19747c23d2ef
Maksym Tsybulskyi <66479202+tomahawksmail@users.noreply.github.com>
2023-05-30 13:30:02 -0700

    Add files via upload

---
+++ one.py
@@ -0,0 +1,30 @@
+from webptools import cwebp
+import os
+import pathlib
+import glob
+path = r'C:\Users\smoke\Downloads\Newfolder'
+desktop = pathlib.Path(path)
+
+
+end = '.webp'
+def obxodFile(path):
+
+    # for file in os.listdir(path):
+    #     file = path+'\\'+file
+    #     newfile = file.split('.')[0]
+    #     newfile = newfile+end
+    #     cwebp(input_image=file, output_image=newfile, option="-q 80", logging="-v")
+    #     print(file, newfile)
+    # print('done')
+    i = 0
+    for file in desktop.rglob("*"):
+        if file.is_file():
+            newfile = str(file).split('.')[0]
+            newfile = newfile + end
+            cwebp(input_image=file, output_image=newfile, option="-q 80", logging="-v")
+            i+=1
+            print(i, newfile, 'done')
+
+
+
+obxodFile(path)
